5278fa01db9ae644ab8af1aaf644021016cb30e6
Nicholas wainaina <wainainanicholas6@gmail.com>
2023-11-22 13:15:18 +0300

    Active Menu Switcher

---
+++ -- Smooth Scroll/script.js
@@ -0,0 +1,12 @@
+//Active Menu Switcher
+
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains("link")) {
+    navList.querySelector(".active").classList.remove("active");
+    navLink.classList.add("active");
+    e.target.parentElement.classList.add("active");
+  }
+});
